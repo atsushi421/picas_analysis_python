39704a0d804721b03c081ce79242cc0079eb76be
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2023-03-07 10:22:55 +0900

    refactor: chain.py

--- src/entities/chain.py
+++ src/entities/chain.py
@@ -3,31 +3,31 @@ from .callback import Callback
 
 
 class Chain:
-    def __init__(self, id: int, sem_prio: int) -> None:
-        self.type: str = "p"
-        self.num_callbacks: int = 0
+    def __init__(self, id: int, sem_priority: int) -> None:
         self.id: int = id
-        self.t_callback: Optional[Callback] = None
-        self.r_callbacks: List[Callback] = []
+        self.sem_priority: int = sem_priority
+
+        self.timer_cb: Optional[Callback] = None
+        self.regular_cbs: List[Callback] = []
+
         self.C: int = 0
         self.T: int = 0
-        self.num_instance: int = 0  # this is only for a non-periodic chain
-        self.trigger_next: bool = False
-        self.next_r_callback_id: int = 0
-        self.sem_priority: int = sem_prio
+
+    @property
+    def num_callbacks(self) -> int:
+        return 1 if self.timer_cb else 0 + len(self.regular_cbs)
 
     def add_callback(self, callback: Callback) -> None:
         if callback.type == "timer":
-            self.t_callback = callback
+            self.timer_cb = callback
             self.T = callback.T
         else:
-            self.r_callbacks.append(callback)
+            self.regular_cbs.append(callback)
 
-        self.num_callbacks = 1 if self.t_callback else 0 + len(self.r_callbacks)
+        # Update chain_C
         self.C += callback.C
+        if self.timer_cb:
+            self.timer_cb.chain_C = self.C
 
-        if self.t_callback:
-            self.t_callback.chain_C = self.C
-
-        for c in self.r_callbacks:  # FIXME
-            c.chain_C = self.C
+        for cb in self.regular_cbs:
+            cb.chain_C = self.C
